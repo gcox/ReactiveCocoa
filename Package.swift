d5c153de279884999fb4ebedf1e7cb1b8c961c2a
George Cox <george@tundaware.com>
2021-02-16 09:30:26 -0500

    Updates Nimble/Quick to latest

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/gcox/ReactiveSwift", .branch("dynamic-lib")),
-        .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "3.1.2"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
     ],
     targets: [
         .target(
